261260586ac6128bf1386935dcf12d10f89d0228
gabzzola <gazzola.co@gmail.com>
2025-06-10 21:00:32 -0300

    feat: adiciona createdAt e updatedAt para ProjectModel

--- src/main/java/com/gabzzola/projectmanager/model/ProjectModel.java
+++ src/main/java/com/gabzzola/projectmanager/model/ProjectModel.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -16,12 +17,29 @@ public class ProjectModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(nullable = false)
     private String name;
 
-    @Column(name = "budget", precision = 19, scale = 4, nullable = false)
+    @Column(nullable = false, precision = 19, scale = 4)
     private BigDecimal budget;
 
-    @Column(name = "deleted")
+    @Column(nullable = false)
     private boolean deleted = false;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
