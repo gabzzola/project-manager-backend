b515f4d233859daf97700f085c4c461a374df5cf
gabzzola <gazzola.co@gmail.com>
2025-06-19 15:29:23 -0300

    chore: configuração inicial do projeto com Spring Boot e Maven

---
+++ src/test/java/com/gabzzola/projectmanager/ProjectManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gabzzola.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
